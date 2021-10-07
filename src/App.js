60a0528110684448841be62f62fea3180642616b
aryangupta701 <garyan447@gmail.com>
2021-10-07 10:59:18 +0530

    using states delete person

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Header from './components/Header';
 import {Component} from "react";
 import Person from './components/Person';
 class App extends Component{
@@ -9,21 +8,38 @@ class App extends Component{
       {name:'Evan' ,age:'21'},
       {name:'Ben' ,age:'22'}
     ],
-    otherstate:'some other state',
     showperson: false
+
   }
-  togglebar= () =>
-  {
-    const doshow=this.state.showperson,
-    this.setState({showperson:!doshow})
-  }
+ toggleperson=()=>{
+    const x=this.state.showperson
+    this.setState({showperson: !x})
+    console.log(this.state.showperson)
+ }
+deleteperson=(index)=>{
+const persons= this.state.persons
+persons.splice(index,1)
+this.setState({persons:persons})
+}
  render(){
+   let persons=null
+   console.log(this.state.showperson)
+   if(this.state.showperson)
+   {
+     persons=(
+       
+       <div>
+         { this.state.persons.map((person,index)=>{
+       return <Person name={person.name} age={person.age} click={()=>this.deleteperson(index)}/>
+         })}
+       </div>
+     )
+    }
+   
   return (
     <div className="App">
-      <button className="btn btn-success" onClick="togglebar" style={{margin:'10px'}}>Toggle Bar</button>
-      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      <button className="btn btn-success" onClick={this.toggleperson} style={{margin:'10px'}}>Toggle Bar</button>
+     {persons}
     </div>
   )
   }
--- src/components/Person.js
+++ src/components/Person.js
@@ -1,15 +1,15 @@
-import react from "react"
+
 const boxstyle = {
     
     margin:'50px',
     padding:'10px',
-    border:'2px solid black' ,
+    border:'2px solid black' 
 
 }
 const person= (props) =>{
 return(
     <div style={boxstyle}>
-    <p>hello ! my name is {props.name} and I am {props.age} years old.</p>
+    <p onClick={props.click}>hello ! my name is {props.name} and I am {props.age} years old.</p>
     <input type="text"></input>
 </div>
 )
